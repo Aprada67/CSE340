ee17ebf27bfb251cd739410d2d04f160d2d1310d
Alberto Prada <albertojprada10@gmail.com>
2025-07-25 16:04:46 +0100

    Finish individual 4

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -25,6 +25,9 @@ async function getInventoryByClassificationId(classification_id) {
   }
 }
 
+/* ***************************
+ *  Get a vehicle by its inventory id
+ * ************************** */
 async function getVehicleById(inv_id) {
   try {
     const data = await pool.query(
@@ -42,11 +45,52 @@ async function getVehicleById(inv_id) {
   }
 }
 
+/* ***************************
+ *  Add a new classification
+ * ************************** */
+async function addClassification(classification_name) {
+  try {
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *"
+    const result = await pool.query(sql, [classification_name])
+    return result.rows[0]
+  } catch (error) {
+    console.error("addClassification error:", error)
+    throw error
+  }
+}
 
-module.exports = { getClassifications }
-module.exports = { getClassifications, getInventoryByClassificationId }
+async function addInventoryItem(item) {
+  try {
+    const sql = `INSERT INTO inventory 
+      (classification_id, inv_make, inv_model, inv_year, inv_price, inv_miles, inv_color, inv_image, inv_thumbnail, inv_description)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
+
+    const values = [
+      item.classification_id,
+      item.inv_make,
+      item.inv_model,
+      item.inv_year,
+      item.inv_price,
+      item.inv_miles,
+      item.inv_color,
+      item.inv_image,
+      item.inv_thumbnail,
+      item.inv_description,
+    ]
+
+    const result = await pool.query(sql, values)
+    return result.rowCount > 0
+  } catch (error) {
+    console.error("addInventoryItem error: ", error)
+    throw error
+  }
+}
+
+
 module.exports = {
   getClassifications,
   getInventoryByClassificationId,
-  getVehicleById
+  getVehicleById,
+  addClassification,
+  addInventoryItem
 }
