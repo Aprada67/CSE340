c90e0ebe7c4d93b3e8174e9b310d575798add61e
Alberto Prada <albertojprada10@gmail.com>
2025-07-25 13:03:16 +0100

    Finish team activity

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,7 @@
 // Required utilities module
 const utilities = require("../utilities")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 
 // *************************************
@@ -12,6 +13,7 @@ async function buildLogin(req, res, next) {
         res.render("account/login", {
             title: "Login",
             nav,
+            errors: null,
         });
     } catch (error) {
         next(error)
@@ -39,32 +41,48 @@ async function buildRegister(req, res, next) {
 *  Process Registration
 * *************************************** */
 async function registerAccount(req, res) {
-  let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+    let nav = await utilities.getNav()
+    const { account_firstname, account_lastname, account_email, account_password } = req.body
 
-  const regResult = await accountModel.registerAccount(
-    account_firstname,
-    account_lastname,
-    account_email,
-    account_password
-  )
+    // Hash the password before storing
+    let hashedPassword
+    try {
+        // regular password and cost (salt is generated automatically)
+        hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+        req.flash("notice", 'Sorry, there was an error processing the registration.')
+        res.status(500).render("account/register", {
+            title: "Registration",
+            nav,
+            errors: null,
+        })
+    }
 
-  if (regResult?.rows?.length > 0) {
-    req.flash(
-      "notice",
-      `Congratulations, you're registered ${account_firstname}. Please log in.`
+    const regResult = await accountModel.registerAccount (
+        account_firstname,
+        account_lastname,
+        account_email,
+        hashedPassword
     )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-    })
-  } else {
-    req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
-      title: "Registration",
-      nav,
-    })
-  }
+
+    if (regResult?.rows?.length > 0) {
+        req.flash(
+            "notice",
+            `Congratulations, you're registered ${account_firstname}. Please log in.`
+        )
+        res.status(201).render("account/login", {
+            title: "Login",
+            nav,
+            errors: null,
+        })
+    } else {
+        req.flash("notice", "Sorry, the registration failed.")
+        res.status(501).render("account/register", {
+            title: "Registration",
+            nav,
+            errors: null,
+        })
+    }
 }
 
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -11,7 +13,33 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Registration route to handle form POST
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+/*
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+*/
+
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send("Login process")
+  }
+)
 
 
 // Export the router
